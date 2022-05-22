2c66c0d1e4534d0570af00baf70eb250859e804e
RasTado <crteamx@gmail.com>
2022-05-23 00:02:51 +0300

    Home Work 1

--- HomeWork.py
+++ HomeWork.py
@@ -10,7 +10,7 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
-    def rate_hw_s(self, lectur, course, grade):
+    def rate_hw(self, lectur, course, grade):
         if isinstance(lectur, Lecturer) and course in lectur.courses_attached and course in self.courses_in_progress:
             if course in lectur.grades:
                 lectur.grades[course] += [grade]
@@ -18,6 +18,31 @@ class Student:
                 lectur.grades[course] = [grade]
         else:
             return 'Error'
+
+    def __avrg_grade(self):
+        sum_mar_course = 0
+        count_mar_course = 0
+        for marks in self.grades.values():
+            sum_mar = 0
+            count_mar = 0
+            for mark in marks:
+                sum_mar += mark
+                count_mar += 1
+            sum_mar_course += sum_mar
+            count_mar_course += count_mar
+        if count_mar_course > 0:
+            avrg_gr = round((sum_mar_course / count_mar_course), 1)
+            return avrg_gr
+        else:
+            return('-')
+
+    def __str__(self):
+        res = f'''
+Имя: {self.name}
+Фамилия: {self.surname}
+Средняя оценка за домашние задания: {self.__avrg_grade()}
+'''
+        return res
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,6 +55,30 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __avrg_grade(self):
+        sum_mar_course = 0
+        count_mar_course = 0
+        for marks in self.grades.values():
+            sum_mar = 0
+            count_mar = 0
+            for mark in marks:
+                sum_mar += mark
+                count_mar += 1
+            sum_mar_course += sum_mar
+            count_mar_course += count_mar
+        if count_mar_course > 0:
+            avrg_gr = round((sum_mar_course / count_mar_course), 1)
+            return avrg_gr
+        else:
+            return('-')
+
+    def __str__(self):
+        res = f'''
+Имя: {self.name}
+Фамилия: {self.surname}
+Средняя оценка за лекции: {self.__avrg_grade()}
+'''
+        return res
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -40,25 +89,31 @@ class Reviewer(Mentor):
         else:
             return 'Error'
 
+    def __str__(self):
+        res = f'''
+Имя: {self.name}
+Фамилия: {self.surname}
+'''
+        return res
 
-student_1 = Student('Ivan', 'Ivanov', 'm')
+
+student_1 = Student('Ivan', 'Ivajov', 'm')
 lectur_1 = Lecturer('Jacky', 'Chan')
+reviever_1 = Reviewer('Brus', 'Li')
 
 student_1.courses_in_progress += ['Java']
-
+student_1.courses_in_progress += ['Python']
 lectur_1.courses_attached += ['Java']
+lectur_1.courses_attached += ['Python']
+reviever_1.courses_attached += ['Java']
+reviever_1.courses_attached += ['Python']
+reviever_1.rate_hw(student_1, 'Python', 10)
+reviever_1.rate_hw(student_1, 'Java', 3)
+student_1.rate_hw(lectur_1, 'Java', 10)
+student_1.rate_hw(lectur_1, 'Python', 5)
 
-student_1.rate_hw_s(lectur_1, 'Java', 10)
-student_1.rate_hw_s(lectur_1, 'Python', 10)
+print(student_1)
 
+print(reviever_1)
 
-print(student_1.name)
-print(student_1.surname)
-print(student_1.gender)
-print(student_1.finished_courses)
-print(student_1.courses_in_progress)
-print(student_1.grades)
-print(lectur_1.name)
-print(lectur_1.surname)
-print(lectur_1.courses_attached)
-print(lectur_1.grades)
+print(lectur_1)
